5d6400ba7bb4fb81b623aaf141a8d6b59d622469
Shawn Gibson <saljgib@gmail.com>
2015-05-27 17:30:07 -0400

    accordion working

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,17 @@
   $(document).ready(function(){
     $('body').prepend(JST['application']());
   });
+
+  (function($) {
+
+    var allPanels = $('.accordion > dd').hide();
+
+    $('.accordion > dt > a').click(function() {
+      allPanels.slideUp();
+      $(this).parent().next().slideDown();
+      return false;
+    });
+
+  })(jQuery);
+
 })();
